3fa2636c49b8afa69868d93d6d02219167e23563
emptychair1 <averiearden@gmail.com>
2026-08-19 04:04:09 -0400

    Fix production dashboard missing template

--- dashboard_metrics.py
+++ dashboard_metrics.py
@@ -161,7 +161,7 @@ def dashboard_v2(request: Request):
 
     return core.templates.TemplateResponse(
         request=request,
-        name="dashboard_v2.html",
+        name="dashboard.html",
         context={
             "user": user,
             "shop": shop,
