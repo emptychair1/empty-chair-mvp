14d6cba28bf1c5546daa11a46c29aae4451bcc36
emptychair1 <averiearden@gmail.com>
2026-08-27 13:02:41 -0400

    Register final M4 V1 evaluation routes

--- m4_stress_benchmark_routes.py
+++ m4_stress_benchmark_routes.py
@@ -7,6 +7,7 @@ import app as core
 import m4_stress_benchmark_jobs as jobs
 import m4_stress_benchmark_v3 as stress
 import m4_v1_training_routes  # noqa: F401,E402
+import m4_v1_final_evaluation_routes  # noqa: F401,E402
 from founder_simulation_safety import FounderSimulationSafetyError
 
 app = core.app
