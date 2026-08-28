b59faa8166e65e743a9b63bf60deac70464cc1dd
emptychair1 <averiearden@gmail.com>
2026-08-28 01:18:04 -0400

    Register multichannel launch console

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,7 @@ import m4_dashboard  # noqa: E402,F401
 import demand_acquisition  # noqa: E402,F401
 import demand_channels  # noqa: E402,F401
 import demand_compliance  # noqa: E402,F401
+import demand_launch  # noqa: E402,F401
 import demand_shortlinks  # noqa: E402,F401
 import demand_ui  # noqa: E402,F401
 
