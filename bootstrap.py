569955e39fb2e7e803c7911712d03b7f07f42ffc
emptychair1 <averiearden@gmail.com>
2026-08-30 14:03:34 -0400

    Register Demand content routes in bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -50,6 +50,7 @@ import crybaby_cleanup_fk_patch  # noqa: F401,E402
 import customer_bulk_delete  # noqa: F401,E402
 import contest  # noqa: F401,E402
 import contest_runtime_fix  # noqa: F401,E402
+import demand_content  # noqa: F401,E402
 
 meeting_v2 = None
 meeting_import_error = None
