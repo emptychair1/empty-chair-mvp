baf88ad1c8c6769c34ac02d0f6bfc874f41b0859
emptychair1 <averiearden@gmail.com>
2026-08-30 14:22:52 -0400

    Register global branded 404 handler

--- production_entry.py
+++ production_entry.py
@@ -17,6 +17,7 @@ import contest_runtime_fix  # noqa: F401,E402
 import demand_tattoo_finder  # noqa: F401,E402
 import demand_content  # noqa: F401,E402
 import demand_read_api  # noqa: F401,E402
+import global_404  # noqa: F401,E402
 
 
 app = bootstrap.app
