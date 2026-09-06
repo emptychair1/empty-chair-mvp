23c8a8d6f809396fdb34069625c444e93d49e2c9
emptychair1 <averiearden@gmail.com>
2026-09-06 08:41:57 -0400

    Update Hunter contracts for public-web Instagram resolution

--- hunter/test_hunter.py
+++ hunter/test_hunter.py
@@ -1,4 +1,10 @@
-from collect import detect_phrase, instagram_username, normalize_result_url, relevant
+from collect import (
+    detect_phrase,
+    find_instagram_url,
+    instagram_username,
+    normalize_result_url,
+    relevant_text,
+)
 
 
 def test_detects_real_cancellation_signal():
@@ -9,20 +15,14 @@ def test_rejects_irrelevant_cancellation_context():
     assert detect_phrase("Event cancellation notice for convention vendors") == ""
 
 
-def test_requires_tattoo_and_instagram_context():
-    ok, phrase = relevant(
-        "Tattoo artist had a cancellation tomorrow, flash available",
-        "https://www.instagram.com/example/",
-    )
+def test_requires_tattoo_context():
+    ok, phrase = relevant_text("Tattoo artist had a cancellation tomorrow, flash available")
     assert ok is True
     assert phrase == "had a cancellation"
 
 
-def test_rejects_non_instagram_result():
-    ok, _ = relevant(
-        "Tattoo artist had a cancellation tomorrow",
-        "https://example.com/post",
-    )
+def test_rejects_non_tattoo_cancellation():
+    ok, _ = relevant_text("Restaurant had a cancellation tomorrow")
     assert ok is False
 
 
@@ -37,3 +37,12 @@ def test_does_not_invent_username_from_media_url():
 def test_unwraps_duckduckgo_redirect():
     wrapped = "https://duckduckgo.com/l/?uddg=https%3A%2F%2Fwww.instagram.com%2Ffoo%2F"
     assert normalize_result_url(wrapped) == "https://www.instagram.com/foo/"
+
+
+def test_resolves_instagram_link_from_public_artist_page():
+    html = '<html><a href="https://www.instagram.com/blackbirdtattoo/">Instagram</a></html>'
+    assert find_instagram_url(html) == "https://www.instagram.com/blackbirdtattoo/"
+
+
+def test_resolves_instagram_handle_from_public_text():
+    assert find_instagram_url("", "Follow on Instagram @blackbirdtattoo") == "https://www.instagram.com/blackbirdtattoo/"
