61c159561e76d0294bfa058d94d59df04349a18c
emptychair1 <averiearden@gmail.com>
2026-09-06 11:57:57 -0400

    Wire Hunter operator console into app bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -25,5 +25,6 @@ import demand_compliance  # noqa: E402,F401
 import demand_launch  # noqa: E402,F401
 import demand_shortlinks  # noqa: E402,F401
 import demand_ui  # noqa: E402,F401
+import hunter_operator  # noqa: E402,F401
 
 app = legacy.app
