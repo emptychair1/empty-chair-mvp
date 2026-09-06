a75835de0da14682d665dfe8c98d6a4054e8abf8
emptychair1 <averiearden@gmail.com>
2026-09-06 06:34:00 -0400

    Activate Instagram organic attribution

--- bootstrap.py
+++ bootstrap.py
@@ -33,6 +33,7 @@ import v2_native_finalize_bridge  # noqa: F401,E402
 import v2_instagram_growth  # noqa: F401,E402
 import v2_instagram_api_fix  # noqa: F401,E402
 import v2_instagram_growth_attribution  # noqa: F401,E402
+import v2_instagram_organic  # noqa: F401,E402
 import v2_instagram_content  # noqa: F401,E402
 import v2_instagram_growth_brain  # noqa: F401,E402
 import v2_instagram_growth_report  # noqa: F401,E402
