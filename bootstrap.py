cc37ad732e2dc809d41d71b419006f10eefbab27
emptychair1 <averiearden@gmail.com>
2026-09-07 14:14:11 -0400

    Load Hunter outreach runtime fix

--- bootstrap.py
+++ bootstrap.py
@@ -51,9 +51,10 @@ import v2_hunter_operator_phone_auth  # noqa: F401,E402
 import v2_hunter_crt_ui  # noqa: F401,E402
 import v2_hunter_outreach  # noqa: F401,E402
 import v2_hunter_outreach_primary  # noqa: F401,E402
+import v2_hunter_outreach_runtime_fix  # noqa: F401,E402
 import v2_native_hunter  # noqa: F401,E402
 
-BUILD_ID = "bootstrap-hunter-outreach-primary-20260907"
+BUILD_ID = "bootstrap-hunter-outreach-runtime-fix-20260907"
 
 
 @app.middleware("http")
